bbe4343e1c4cd2e9c72445dfa6fd9e459067388e
erikvw <ew2789@gmail.com>
2018-07-21 17:43:57 +0200

    update setup files

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author_email='ew2789@gmail.com',
     packages=find_packages(),
     url='http://github.com/clinicedc/edc-dashboard',
-    license='GPL licence, see LICENCE',
+    license='GPL license, see LICENSE',
     description='Dashboard classes, templates for clinicedc/edc projects.',
     long_description=README,
     include_package_data=True,
