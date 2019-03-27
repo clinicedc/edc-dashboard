a9e44f86b3906688547f3174ca1eaeaf0e88dc0c
erikvw <ew2789@gmail.com>
2019-03-27 15:58:12 -0500

    use os.path, raise if cannot get_template

--- edc_dashboard/utils.py
+++ edc_dashboard/utils.py
@@ -1,4 +1,12 @@
+import os
+
 from django.conf import settings
+from django.template.loader import get_template
+from django.template.exceptions import TemplateDoesNotExist
+
+
+class EdcTemplateDoesNotExist(Exception):
+    pass
 
 
 def insert_bootstrap_version(**template_data):
@@ -10,10 +18,22 @@ def insert_bootstrap_version(**template_data):
     except AttributeError:
         bootstrap_version = 3
     if bootstrap_version:
-        for key, value in template_data.items():
-            if "/bootstrap" not in value:
-                value = value.split("/")
-                value.insert(1, f"bootstrap{bootstrap_version}")
-                value = "/".join(value)
-            template_data.update({key: value})
+        for key, original_path in template_data.items():
+            try:
+                get_template(original_path)
+            except TemplateDoesNotExist:
+                if "/bootstrap" not in original_path:
+                    base_name = os.path.basename(original_path)
+                    prefix = original_path.split(base_name)[0]
+                    path = os.path.join(
+                        prefix, f"bootstrap{bootstrap_version}", base_name
+                    )
+                    try:
+                        get_template(path)
+                    except TemplateDoesNotExist as e:
+                        raise EdcTemplateDoesNotExist(
+                            f"Template file does not exist. "
+                            f"Tried {original_path} and {path}. Got {e}"
+                        )
+                template_data.update({key: path})
     return template_data
