ab37b747834eb4c5146df346ffa4456aa1b02200
erikvw <ew2789@gmail.com>
2019-06-24 18:59:32 -0500

    do not show apps in admin section by default

--- edc_dashboard/views/error_handlers/sentry.py
+++ edc_dashboard/views/error_handlers/sentry.py
@@ -3,6 +3,4 @@ from django.shortcuts import render
 
 
 def handler500(request, *args, **argv):
-    return render(request, "500.html", {
-        'sentry_event_id': last_event_id(),
-    }, status=500)
+    return render(request, "500.html", {"sentry_event_id": last_event_id()}, status=500)
