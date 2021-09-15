1cfc265080414f635eaef150aa30e3a92e1ddd33
erikvw <ew2789@gmail.com>
2021-09-14 22:12:19 -0500

    auths

--- edc_dashboard/auths.py
+++ edc_dashboard/auths.py
@@ -6,3 +6,7 @@ site_auths.add_post_update_func(remove_permissions_to_edc_dashboard_model)
 site_auths.add_custom_permissions_tuples(
     model="edc_dashboard.dashboard", codename_tuples=dashboard_tuples
 )
+
+
+"edc_dashboard.view_screening_listboard",
+"edc_dashboard.view_subject_listboard",
